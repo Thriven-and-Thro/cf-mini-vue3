f92c2901d5573f26d2d34de649a28569f706af0a
cf <475392583@qq.com>
2022-05-25 23:40:54 +0800

    fix: finsih shallowReadonly

--- src/reactivity/tests/shallowReadonly.spec.ts
+++ src/reactivity/tests/shallowReadonly.spec.ts
@@ -2,9 +2,12 @@ import { isReadonly, shallowReadonly } from "../reactive";
 
 describe("shallowReadonly", () => {
   it("base", () => {
-    const user = { hobbies: [1, 2] };
+    const user = { age: 1, hobbies: [1, 2] };
     const userProxy = shallowReadonly(user);
     expect(isReadonly(userProxy)).toBe(true);
     expect(isReadonly(userProxy.hobbies)).toBe(false);
+    console.warn = jest.fn();
+    userProxy.age++;
+    expect(console.warn).toBeCalled();
   });
 });
