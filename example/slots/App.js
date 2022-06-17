229034c10408407cc01760409abe089d12d12a27
cf <475392583@qq.com>
2022-06-17 23:03:41 +0800

    fix: slot of component

--- example/slots/App.js
+++ example/slots/App.js
@@ -1,7 +1,8 @@
 import { h, renderSlots } from "../../lib/cf-mini-vue.esm.js";
 import { Foo1, Foo2 } from "./Foo.js";
 
-// children为对象传递插槽值
+// children为对象：传递插槽值
+// 对象中的值为函数，且需返回节点
 
 export const App = {
   setup() {},
--- src/runtime-core/heplers/renderSlots.ts
+++ src/runtime-core/heplers/renderSlots.ts
@@ -1,12 +1,12 @@
-import { h } from "../index";
+import { createVNode } from "../vnode";
 
-// 使用插槽函数：从slots中取值
+// 使用插槽函数：从$slots中取值
 export function renderSlots(slots, name = "default", props = {}) {
   const slot = slots[name];
 
   if (slot) {
     if (typeof slot === "function") {
-      return h("div", {}, slot(props));
+      return createVNode("div", {}, slot(props));
     }
   }
 }
